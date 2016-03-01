28011398be602e585f74dd8bbc022814b30abc82
Naum Azeredo <naumazeredo@gmail.com>
2016-03-01 16:32:20 -0300

    Added template.cpp

---
+++ template.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define st first
+#define nd second
+#define mp make_pair
+#define pb push_back
+
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define _ << ", " <<
+
+typedef long long ll;
+typedef long double ld;
+
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9+7;
+
+int main() {
+  return 0;
+}
