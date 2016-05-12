46f2c463e1cd44e6e61c0f9ba14cb750d957c1a6
Naum Azeredo <naumazeredo@gmail.com>
2016-05-12 11:51:46 -0300

    Updated spoj folder

--- spoj/br/mineira/assaltmg.cpp
+++ spoj/br/mineira/assaltmg.cpp
@@ -2,47 +2,34 @@
 #include <bitset>
 using namespace std;
 
+const int INF = 0x3f3f3f3f;
+
 bitset<32> dir[30];
 
-int main()
-{
+int main() {
   int t, m, d, c, key;
   scanf("%d", &t);
 
-  for (int i = 0; i < t; ++i)
-  {
+  for (int i = 0; i < t; ++i) {
     scanf("%d %d", &m, &d);
 
-    for (int j = 0; j < d; ++j)
-    {
+    for (int j = 0; j < d; ++j) {
       dir[j] = bitset<32>();
       scanf("%d", &c);
       for (int k = 0; k < c; ++k)
-      {
-        scanf("%d", &key);
-        dir[j].set(key);
-      }
+        scanf("%d", &key), dir[j].set(key);
     }
 
-    // Complete search
-    int mindirs = 9999;
-    int numsets = (1 << d);
-    for (int j = 0; j < numsets; ++j)
-    {
-      bitset<20> s (j);
+    int mindirs = INF;
+    for (int j = 0; j < (1<<d); ++j) {
+      bitset<20> s(j);
       bitset<32> keys;
-      for (size_t k = 0; k < s.size(); ++k)
-        if (s.test(k))
-          keys |= dir[k];
-
-      if (keys.count() >= m && mindirs > s.count())
-        mindirs = s.count();
+      for (int k = 0; k < 32; ++k) if (s[k]) keys |= dir[k];
+      if (keys.count() >= m) mindirs = min(mindirs, (int)s.count());
     }
 
-    if (mindirs < 100)
-      printf("%d\n", mindirs);
-    else
-      printf("Desastre!\n");
+    if (mindirs < 100) printf("%d\n", mindirs);
+    else printf("Desastre!\n");
   }
 
   return 0;
---
+++ spoj/histogra.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define st first
+#define nd second
+#define mp make_pair
+#define pb push_back
+#define cl(x) memset((x), 0, sizeof(x))
+#define cli(x) memset((x), 63, sizeof(x))
+
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define _ << ", " <<
+
+typedef long long ll;
+typedef long double ld;
+
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+
+const int INF = 0x3f3f3f3f, MOD = 1e9+7;
+const int N = 1e5+5;
+
+int n;
+int h[N], 
+
+int main() {
+  return 0;
+}
