ae5b6e9890f5b99e08d3c9650c3553cd438ab560
Naum Azeredo <naumazeredo@gmail.com>
2016-06-14 20:07:17 -0300

    Updated template

--- template.cpp
+++ template.cpp
@@ -19,7 +19,8 @@ typedef pair<int, pii> piii;
 typedef pair<ll, ll> pll;
 typedef vector<int> vi;
 
-const int INF = 0x3f3f3f3f, MOD = 1e9+7, EPS = 1e-6;
+const ld EPS = 1e-6, PI = acos(-1);
+const int INF = 0x3f3f3f3f, MOD = 1e9+7;
 const int N = 1e5+5;
 
 int main() {
