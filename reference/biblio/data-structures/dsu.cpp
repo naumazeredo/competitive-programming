88d3f4ded8eec3020840a26354e01c96e603d958
Naum Azeredo <naumazeredo@gmail.com>
2016-09-08 21:34:11 -0300

    Updated biblio

--- reference/biblio/data-structures/dsu.cpp
+++ reference/biblio/data-structures/dsu.cpp
@@ -6,6 +6,5 @@ int find(int a) { return par[a] == a ? a : par[a] = find(par[a]); }
 void unite(int a, int b) {
   if ((a = find(a)) == (b = find(b))) return;
   if (sz[a] < sz[b]) swap(a, b);
-  par[b] = a;
-  sz[a] += sz[b];
+  par[b] = a; sz[a] += sz[b];
 }
--- reference/biblio/graphs/articulation-points-bridges.cpp
+++ reference/biblio/graphs/articulation-points-bridges.cpp
@@ -1,19 +1,18 @@
 // Articulation points and Bridges O(V+E)
-int par[N], art[N], low[N], num[N], cnt;
+int par[N], art[N], low[N], num[N], ch[N], cnt;
 
 void articulation(int u) {
   low[u] = num[u] = ++cnt;
   for (int v : adj[u]) {
     if (!num[v]) {
-      par[v] = u;
+      par[v] = u; ch[u]++;
       articulation(v);
       if (low[v] >= num[u]) art[u] = 1;
       if (low[v] >  num[u]) // u-v bridge
-    } else if (v != par[u]) {
-      low[u] = min(low[u], num[v]);
     }
+    if (v != par[u]) low[u] = min(low[u], low[v]);
   }
 }
 
 // root special case
-art[root] &= adj[i].size()>1;
+art[0] = ch[0]>1;
