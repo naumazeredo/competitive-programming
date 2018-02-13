fc5b41c57c8ec8ccc14232ae9357601d2641167a
Naum Azeredo <naumazeredo@gmail.com>
2018-02-13 22:51:45 +0000

    Remove reference. Use imeplusplus/icpc-notebook

--- reference/biblio/graphs/dfs.cpp
+++
@@ -1,9 +0,0 @@
-// Depth First Search O(V+E)
-vi adj[N];
-int vis[N];
-
-void dfs(int u) {
-  vis[u]=1;
-  for (int v : adj[u]) if (!vis[v])
-    dfs(v);
-}
--- reference/biblio/misc/builtin.cpp
+++
@@ -1,6 +0,0 @@
-__builtin_ctz(x) // trailing zeroes
-__builtin_clz(x) // leading zeroes
-__builtin_popcount(x) // # bits set
-__builtin_ffs(x) // index(LSB) + 1 [0 if x==0]
-
-// Add ll to the end for long long [__builtin_clzll(x)]
